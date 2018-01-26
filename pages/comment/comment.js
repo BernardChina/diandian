34966de330cbdaae3303cf71c710855dd8f454e5
刘勇强 <1054462882@qq.com>
2018-01-26 14:58:15 +0800

    [f]增加评论窗口

--- pages/comment/comment.js
+++ pages/comment/comment.js
@@ -1,15 +1,13 @@
-// pages/Detail/index.js
+// pages/comment/comment.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
-    name: '此时此刻',
-    author: '许巍',
-    src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+
   },
+
   /**
    * 生命周期函数--监听页面加载
    */
@@ -21,7 +19,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    this.videoContext = wx.createVideoContext('myVideo')
+
   },
 
   /**
@@ -65,18 +63,9 @@ Page({
   onShareAppMessage: function () {
 
   },
-
-  shareTap: function () {
-    wx.showToast({
-      title: '请点击右上角进行分享',
-      icon: 'none',
-      duration: 2000
-    })
-  },
-  messageTap: function () {
-    wx.navigateTo({
-      url: '../comment/comment'
+  comfirm: function () {
+    wx.navigateBack({
+      delta: 1
     })
   }
-
 })
