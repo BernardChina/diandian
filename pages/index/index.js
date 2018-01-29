b260f9cf4e2c6afc14118e90c8703d28df79f62c
刘勇强 <1054462882@qq.com>
2018-01-29 10:43:01 +0800

    [f]增加广告

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,11 @@
 var app = getApp()
 Page({
   data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
     videosModel: [{
       poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
       src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
@@ -48,15 +53,10 @@ Page({
   },
   onLoad: function (option) {
     console.log('onLoad')
-    console.log(option);
-    var that = this
 
   },
   onReady: function () {
     console.log('onReady')
-    this.setData({
-      motto: 'hello world'
-    })
   },
   onShow: function () {
     console.log('onShow')
@@ -72,7 +72,7 @@ Page({
     })
   },
   videoTap: function () {
-    
+
   }
 
 })
